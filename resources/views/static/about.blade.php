c3ae540d0f9b0c40f8cd13fb7dee9ee9ee8e8b1e
Markov7322 <info@onlymk.ru>
2025-06-05 18:05:10 +0300

    Добавлены страницы 'О проекте' и 'Контакты'

---
+++ resources/views/static/about.blade.php
@@ -0,0 +1,9 @@
+<x-app-layout>
+    <div class="max-w-7xl mx-auto p-6">
+        <h1 class="text-2xl font-bold mb-4">О проекте</h1>
+        <p class="text-gray-700 dark:text-gray-300">
+            Это демонстрационная страница проекта. Здесь вы можете разместить
+            информацию о целях и возможностях сервиса.
+        </p>
+    </div>
+</x-app-layout>
